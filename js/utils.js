6122e883706acde0b1896136c124f8947bf990fb
mb-refactory <162983326+mb-refactory@users.noreply.github.com>
2024-04-29 09:17:19 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -91,7 +91,7 @@ function getLanguage() {
 
 async function getTranslation(language, key) {
   try {
-    const response = await fetch('JS/translation.json');
+    const response = await fetch('js/translation.json');
     const data = await response.json();
     return data[language][key];
   } catch (error) {
